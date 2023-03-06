425e9a07efc592b5783dd3bf29a2b2ce9cdcd9e9
boyan98 <118880676+boyan98@users.noreply.github.com>
2023-03-06 12:34:05 +0200

    Create 02.Radians_to_degrees.js

---
+++ First_Steps_JS_Exercise/02.Radians_to_degrees.js
@@ -0,0 +1,8 @@
+function radiansToDegrees(Input){
+    let offsetRadians = Number(Input[0]);
+    let degrees = offsetRadians *180/Math.PI
+    console.log(degrees);
+    
+}
+
+radiansToDegrees(["6.2832"])
